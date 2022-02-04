389211428f5e93257dd5b2d7e8678b52810762f7
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-02-04 11:50:20 +0500

    js add

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const loader = document.querySelector('.loader')
+const wrapper = document.querySelector('.wrapper')
+const auf = document.querySelector('.auf')
+const motiv = document.querySelector('#motiv')
+const btn = document.querySelector('#btn')
+
+setTimeout(() => {
+    loader.style.display = 'none'
+    wrapper.style.display = 'block'    
+    auf.style.opacity = 1    
+}, 1500)
+
+btn.addEventListener('click', () => {
+    fetch("https://api.adviceslip.com/advice")
+    .then((result) => result.json())
+    .then((data) => (motiv.textContent = data.slip.advice));
+})
